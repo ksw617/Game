d56eda88c17ca59b574d6f209f7ac1f2b05e5757
ksw617 <45592229+ksw617@users.noreply.github.com>
2021-06-02 18:43:27 +0900

    update camera

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 
@@ -23,8 +25,11 @@ public class CameraController : MonoBehaviour, PlayerInputAction.ICameraActions
 
     [SerializeField] private float distance;
     [SerializeField] private float myDistance;
+
+    private bool processing;
     private void Awake()
     {
+        processing = false;
         xRotation = 0f;
         parent = transform.parent;
         playerInputAction = new PlayerInputAction();
@@ -53,34 +58,35 @@ public class CameraController : MonoBehaviour, PlayerInputAction.ICameraActions
         playerInputAction.Camera.Disable();
     }
 
-    void FixedUpdate()
-    {
-        newObj.transform.position = transform.position;
-        newObj.transform.LookAt(player);
-
-        Vector3 fwd = newObj.transform.TransformDirection(Vector3.forward);
-
-        distance = Vector3.Distance(newObj.transform.position, player.position);
-
-        RaycastHit hit;
-        if (Physics.Raycast(newObj.transform.position, fwd, out hit, distance, layerMask))
-        {
-
-            newObj.transform.Translate(Vector3.forward * Time.fixedDeltaTime * 5f);
-            transform.position = newObj.transform.position;
-        }
-        else // 수정할꺼임
-        {
-           
-            if (distance < myDistance)
-            {
-                newObj.transform.Translate(Vector3.back * Time.fixedDeltaTime * 5f);
-                transform.position = newObj.transform.position;
-            }
-        }
-
-
-    }
+    //void FixedUpdate()
+    //{
+    //    newObj.transform.position = transform.position;
+    //    newObj.transform.LookAt(player);
+    //
+    //    Vector3 fwd = newObj.transform.TransformDirection(Vector3.forward);
+    //
+    //    distance = Vector3.Distance(newObj.transform.position, player.position);
+    //
+    //    RaycastHit hit;
+    //    if (Physics.Raycast(newObj.transform.position, fwd, out hit, distance, layerMask))
+    //    {
+    //
+    //        newObj.transform.Translate(Vector3.forward * Time.fixedDeltaTime * 5f);
+    //        transform.position = newObj.transform.position;
+    //       
+    //    }
+    //    else // 수정할꺼임
+    //    {
+    //
+    //        if (myDistance - distance > 0.1f )
+    //        {
+    //            newObj.transform.Translate(Vector3.back * Time.fixedDeltaTime * 5f);
+    //            transform.position = newObj.transform.position;
+    //        }
+    //    }
+    //
+    //
+    //}
 
     public void OnRotate(InputAction.CallbackContext context)
     {
@@ -95,11 +101,59 @@ public class CameraController : MonoBehaviour, PlayerInputAction.ICameraActions
             parent.Rotate(Vector3.up * axis.x * sensitivity);
 
         }
- 
+
+        newObj.transform.position = transform.position;
+        newObj.transform.LookAt(player);
+
+        Vector3 fwd = newObj.transform.TransformDirection(Vector3.forward);
+
+        distance = Vector3.Distance(newObj.transform.position, player.position);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(newObj.transform.position, fwd, out hit, distance, layerMask) && !processing)
+        {
+            processing = true;
+            StartCoroutine("Test", fwd);
+    //
+    //        newObj.transform.Translate(Vector3.forward * Time.fixedDeltaTime * 5f);
+    //        transform.position = newObj.transform.position;
+    //       
+        }
+
     }
+    IEnumerator Test(Vector3 fwd)
+    {
+        RaycastHit hit;
+
+        Debug.Log("Start");
+
+        while (Physics.Raycast(newObj.transform.position, fwd, out hit, distance, layerMask))
+        {
+            newObj.transform.position = transform.position;
+            newObj.transform.LookAt(player);
+
+            fwd = newObj.transform.TransformDirection(Vector3.forward);
+
+            newObj.transform.Translate(Vector3.forward * Time.fixedDeltaTime * 5f);
+            transform.position = newObj.transform.position;
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        Debug.Log("End");
+        processing = false;
+
+        yield return null;
+    }
+
+
 
     public void OnZoomInOut(InputAction.CallbackContext context)
     {
+        newObj.transform.position = transform.position;
+        newObj.transform.LookAt(player);
+
         float middleMouseY = context.ReadValue<float>();
         if (middleMouseY > 0)
         {
