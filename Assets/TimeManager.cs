0fcf4d16f0ba1615157f5fb533fcd6d2f1fb10ab
ksw617 <45592229+ksw617@users.noreply.github.com>
2021-06-07 20:01:51 +0900

    update TimeManager

--- Assets/TimeManager.cs
+++ Assets/TimeManager.cs
@@ -12,42 +12,82 @@ public class TimeManager : MonoBehaviour
     public int speed = 1;
 
     [Header("Time")]
-    public int dayToSecond;
+    public int gameDayToSecond = 1;
 
     private int dayToMonth = 31;
 
     [SerializeField] private float time;
     private float dayRate;
 
+    int year;
+    int month;
+    int day;
+
+    const int dayToSecond = 86400;
+
     private void Awake()
     {
-        dayRate = 86400f / dayToSecond;
+        dayRate = (float)dayToSecond / gameDayToSecond;
+    }
+
+    private void Start()
+    {
+        year = 0;
+        month = 1;
+        day = 1;
     }
 
     private void FixedUpdate()
+    {
+        CalculateDate();
+     
+       // sun.Rotate(Vector3.right * (7.2f / dayToSecond) * speed);
+    }
+
+    void CalculateDate()
     {
         time += Time.fixedDeltaTime * speed * dayRate;
         int second = (int)time % 60;
         int min = ((int)time / 60 % 60);
         int hour = ((int)time / 3600 % 24);
-        int year = (int)time / 31536000;
-  
-        int month = (int)time / (86400 * dayToMonth) % 12;
-        int day = (int)time / 86400 % dayToMonth;
-tu
-        dayToMonth = GetDayToMonth(year, month);
 
-        sun.Rotate(Vector3.right * (7.2f / dayToSecond) * speed);
+        if(time >= 3600 * 24) 
+        {
+            Debug.Log("test");
+            time = 0f;
+           
+            day++;
+            if (day == dayToMonth) // dayToMonth : 31 30 29 28
+            {
+                day = 1;
+                month++;
+                dayToMonth = GetDayToMonth();
+                if (month == 13)
+                {
+                    month = 0;
+                    year++;
+                }
+              
+            }
+        }
+
+       
+
+       
+
+        text.text = $"{year}³â{month}¿ù{day}ÀÏ \n {hour}:{min}:{second}";
     }
 
-    int GetDayToMonth(int year, int month)
+    //                      0       1
+    int GetDayToMonth()
     {
+        //   
         if (month <= 7)
         {
 
             if (month == 2)
             {
-                return GetLeftYear(year);
+                return GetLeftYear();
             }
 
 
@@ -73,7 +113,7 @@ tu
         }
     }
 
-    int GetLeftYear(int year)
+    int GetLeftYear()
     {
         if (year % 4 == 0)
         {
