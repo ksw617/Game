48fda91bea2c45678cee42d02d7d2915fd30fb5c
ksw617 <ksw617@gmail.com>
2024-06-18 17:40:11 +0900

    update day05

--- Client/MainGame.cpp
+++ Client/MainGame.cpp
@@ -9,22 +9,18 @@ void MainGame::Init(HWND hwnd, int width, int height, bool windowed)
 {
 	Engine::Get().Init(hwnd, width, height, windowed);
 
-	//정점 데이터 초기화(삼각형 정의)
 	vector<Vertex> vec(3);
 	vec[0].pos = XMFLOAT3(0.f, 0.5f, 0.5f); 
-	vec[0].color = XMFLOAT4(1.f, 0.f, 0.f, 1.f); //빨강색 정점
+	vec[0].color = XMFLOAT4(1.f, 0.f, 0.f, 1.f); 
 	vec[1].pos = XMFLOAT3(0.5f, -0.5f, 0.5f);
-	vec[1].color = XMFLOAT4(0.f, 1.f, 0.f, 1.f); //초록색 정점
+	vec[1].color = XMFLOAT4(0.f, 1.f, 0.f, 1.f);
 	vec[2].pos = XMFLOAT3(-0.5f, -0.5f, 0.5f);
-	vec[2].color = XMFLOAT4(0.f, 0.f, 1.f, 1.f); //파란색 정점
+	vec[2].color = XMFLOAT4(0.f, 0.f, 1.f, 1.f);
 
-	//매쉬 초기화
 	mesh->Init(vec);
 
-	//쉐이더 초기화
 	shader->Init(L"..\\Resources\\Shader\\Default.hlsli");
 
-	//명령 대기열 동기화 대기
 	Engine::Get().GetCmdQueue()->WaitSync();
 	
 
@@ -32,15 +28,25 @@ void MainGame::Init(HWND hwnd, int width, int height, bool windowed)
 
 void MainGame::Tick()
 {
-	//랜더링 시작
 	Engine::Get().RenderBegin();
 
-	//쉐이더 업데이트
 	shader->Update();
 
-	//메쉬 랜더링
-	mesh->Render();
+	{
+		XMFLOAT4 transform(0.2f, 0.f, 0.f, 0.f);
+		mesh->SetTransform(transform);
 
-	//랜더링 종료
+		mesh->Render();
+
+	}
+	
+
+	{
+		XMFLOAT4 transform(0.f, 0.75f, 0.f, 0.f);
+		mesh->SetTransform(transform);
+
+		mesh->Render();
+	}
+
 	Engine::Get().RenderEnd();
 }
--- Engine/Engine.h
+++ Engine/Engine.h
@@ -1,11 +1,11 @@
 #pragma once
-//포함
 #include "Device.h"
 #include "CommandQueue.h"
 #include "SwapChain.h"
 #include "RootSignature.h"
 #include "Mesh.h"
 #include "Shader.h"
+#include "ConstantBuffer.h"
 
 class Engine
 {
@@ -36,11 +36,16 @@ private:
 	shared_ptr<CommandQueue> cmdQueue; 
 	shared_ptr<SwapChain> swapChain; 
 	shared_ptr<RootSignature> rootSignature;
+	//cosntBuffer 포인터
+	shared_ptr<ConstantBuffer> constBuffer;
 public:
 	shared_ptr<Device> GetDevice() { return device; }
 	shared_ptr<CommandQueue> GetCmdQueue() { return cmdQueue; }
 	shared_ptr<SwapChain> GetSwapChain() { return swapChain; }
 	shared_ptr<RootSignature> GetRootSignature() { return rootSignature; }
+	//Get함수
+	shared_ptr<ConstantBuffer> GetConstBuffer() { return constBuffer; }
+
 public:
 	void Init(HWND _hwnd, int _width, int _height, bool _windowed);
 	void Render();
