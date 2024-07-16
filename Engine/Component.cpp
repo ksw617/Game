3c565e8ab8af52d66586386ad773ac0342d9bddb
ksw617 <ksw617@gmail.com>
2024-07-16 17:45:13 +0900

    update

--- Engine/Component.cpp
+++ Engine/Component.cpp
@@ -3,7 +3,7 @@
 #include "GameObject.h"
 
 //타입 초기화
-Component::Component(COMPONENT_TYPE _type) : type(_type)
+Component::Component(COMPONENT_TYPE _type) : type(_type), Object(OBJECT_TYPE::COMPONENT)
 {
 }
 
@@ -13,13 +13,10 @@ Component::~Component()
 
 shared_ptr<GameObject> Component::GetGameObject()
 {
-    //weak_ptr 잠금하여 shared_ptr<GameObject>를 반환 
     return gameObject.lock();
 }
 
 shared_ptr<Transform> Component::GetTransform()
 {
-    //weak_ptr를 잠금하여 shared_ptr<GameObject>를 변환은 GetTransfrom함수를 이용해서
-    //Transform을 반환
     return gameObject.lock()->GetTransform();
 }
